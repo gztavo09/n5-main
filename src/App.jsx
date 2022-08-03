b79fd4b8692673d11c30539f1ee86a2fe94222df
Gustavo Arias <gjarias@arqsoft.com.pe>
2022-08-02 19:06:05 -0500

    adjust term

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import GlobalStyle from "./styles/globalStyle";
 import Container from "./styles/container/Container";
 
 /* COMPONENTES */
-import Nav from "./components/navigation/Navigator"
+import Nav from "./components/navigation/nav"
 
 /* VISTAS */
 import Home from './views/Home';
